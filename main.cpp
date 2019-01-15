b693de1d58bf0db212b61a591026abbcedbdf29e
nelson.escobar <nelson.holic@gmail.com>
2019-01-15 15:37:10 -0500

    creating method using raw pixels data

--- main.cpp
+++ main.cpp
@@ -4,6 +4,30 @@
 using namespace std;
 using namespace Magick;
 
+void desaturateImage(Image *image) {
+    size_t w = image->columns();
+    size_t h = image->rows();
+
+    // get a "pixel cache" for the entire &image
+    PixelPacket *pixels = image->getPixels(0, 0, w, h);
+
+    // now you can access single pixels like a vector
+    int row = 0;
+    int column = 0;
+    int desaturateColor = 0;
+
+
+    for (row = 0; row < h; row++) {
+        for (column = 0; column < w; column++) {
+            desaturateColor = (pixels[w * row + column].red + pixels[w * row + column].green  + pixels[w * row + column].blue) / 3;
+            pixels[w * row + column] = Magick::Color(desaturateColor, desaturateColor, desaturateColor);
+        }
+    }
+
+    image->syncPixels();
+}
+
+
 int main(int argc,char **argv)
 {
     InitializeMagick(*argv); // Create base image
@@ -21,14 +45,16 @@ int main(int argc,char **argv)
 
         string result_filename;
 
+
+
         if(argc >= 3) {
             result_filename = argv[2];
         } else {
             result_filename = "result_image.png";
         }
 
+        desaturateImage(&image);
 
-        image.type( Magick::GrayscaleType );
         image.write(result_filename);
     }
     catch( Exception &error_ )
