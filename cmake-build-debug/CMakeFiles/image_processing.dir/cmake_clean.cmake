b90bcf8bfa618830efbe7a7d16fe3f3f2d829518
nelson.escobar <nelson.holic@gmail.com>
2019-01-15 17:07:32 -0500

    adding binary

---
+++ cmake-build-debug/CMakeFiles/image_processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_processing.dir/main.cpp.o"
+  "image_processing.pdb"
+  "image_processing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_processing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
